8aa3c2090c4198e01448701d5d1143d9bc2458c7
Billie Zhang <billiecoder@gmail.com>
2015-12-13 14:24:38 +0800

    i

--- src/components/User/Profile.js
+++ src/components/User/Profile.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import NavBar from './NavBar';
-import Footer from './Footer';
+import UserInfo from './UserInfo';
+import Repos from './Repos';
+import helpers from '../utils/helpers';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      repos: [],
+      bio: {},
+    };
+  }
+
+  componentDidMount() {
+    const userName = this.props.params.userName;
+
+    helpers.getGithubInfo(userName)
+      .then((data) => {
+        this.setState({
+          bio: data.bio,
+          repos: data.repos
+        });
+      });
+  }
+
   render() {
+    const userName = this.props.params.userName;
     return (
       <div>
-        <NavBar />
-        <Footer />
+        <UserInfo userName={userName} userInfo={this.state.bio} />
+        <Repos userName={userName} repos={this.state.repos} />
       </div>
     );
   }
