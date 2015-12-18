58622fdf7929f68559e409f4cc6f57a55ee83643
Billie Zhang <billiecoder@gmail.com>
2015-12-18 23:10:38 +0800

    i

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,9 @@ const Home = React.createClass({
   render() {
     return (
       <div className="home">
+        <div className="slogan">
+          welcome to stage two
+        </div>
       </div>
     );
   }
