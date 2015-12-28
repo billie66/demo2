1851f1971752b7494cc27d9005214444f5b9d879
Billie Zhang <billiecoder@gmail.com>
2015-12-28 09:22:55 +0800

    step01

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="slogan">
+          welcome to stage two
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
